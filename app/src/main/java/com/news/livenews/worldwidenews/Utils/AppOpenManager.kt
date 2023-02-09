d26191f12eb7a2accf409baaaf219879dc88b3d8
Fahamin <fahamin00@gmail.com>
2023-02-09 15:34:36 +0600

    done

--- app/src/main/java/com/news/livenews/worldwidenews/Utils/AppOpenManager.kt
+++ app/src/main/java/com/news/livenews/worldwidenews/Utils/AppOpenManager.kt
@@ -1,168 +1,122 @@
-package com.news.livenews.worldwidenews.Utils;
+package com.news.livenews.worldwidenews.Utils
 
-import static androidx.lifecycle.Lifecycle.Event.ON_START;
+import android.app.Activity
+import android.app.Application.ActivityLifecycleCallbacks
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.appopen.AppOpenAd
+import com.google.android.gms.ads.appopen.AppOpenAd.AppOpenAdLoadCallback
+import com.news.livenews.worldwidenews.MyApplication
 
-import android.app.Activity;
-import android.app.Application;
-import android.os.Bundle;
-import android.util.Log;
+/** Prefetches App Open Ads.  */
+class AppOpenManager(private val myApplication: MyApplication) : LifecycleObserver,
+    ActivityLifecycleCallbacks {
+    //  private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/3419835294";
+    private var appOpenAd: AppOpenAd? = null
+    private val currentActivity: Activity? = null
+    private var loadCallback: AppOpenAdLoadCallback? = null
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
-import androidx.lifecycle.ProcessLifecycleOwner;
-
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.appopen.AppOpenAd;
-import com.news.livenews.worldwidenews.MyApplication;
-
-/** Prefetches App Open Ads. */
-public class AppOpenManager implements LifecycleObserver, Application.ActivityLifecycleCallbacks{
-    private static final String LOG_TAG = "AppOpenManager";
-
-   private static final String AD_UNIT_ID = "ca-app-pub-6013605396446078/8842820429";
-
-
-  //  private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/3419835294";
-
-    private AppOpenAd appOpenAd = null;
-    private Activity currentActivity;
-    private AppOpenAd.AppOpenAdLoadCallback loadCallback;
-    private static boolean isShowingAd = false;
-    private final MyApplication myApplication;
-
-
-    /** Constructor */
-    public AppOpenManager(MyApplication myApplication) {
-        this.myApplication = myApplication;
-        this.myApplication.registerActivityLifecycleCallbacks(this);
-        ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
+    /** Constructor  */
+    init {
+        myApplication.registerActivityLifecycleCallbacks(this)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
-    /** LifecycleObserver methods */
-    @OnLifecycleEvent(ON_START)
-    public void onStart() {
-        showAdIfAvailable();
-        Log.d(LOG_TAG, "onStart");
+    /** LifecycleObserver methods  */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+        showAdIfAvailable()
+        Log.d(LOG_TAG, "onStart")
     }
 
-    /** Request an ad */
-    public void fetchAd() {
+    /** Request an ad  */
+    fun fetchAd() {
         // Have unused ad, no need to fetch another.
-        if (isAdAvailable()) {
-            return;
+        if (isAdAvailable) {
+            return
         }
+        loadCallback = object : AppOpenAdLoadCallback() {
+            /**
+             * Called when an app open ad has loaded.
+             *
+             * @param ad the loaded app open ad.
+             */
+            override fun onAdLoaded(ad: AppOpenAd) {
+                appOpenAd = ad
+            }
 
-        loadCallback =
-                new AppOpenAd.AppOpenAdLoadCallback() {
-                    /**
-                     * Called when an app open ad has loaded.
-                     *
-                     * @param ad the loaded app open ad.
-                     */
-                    @Override
-                    public void onAdLoaded(AppOpenAd ad) {
-                        AppOpenManager.this.appOpenAd = ad;
-                    }
-
-                    /**
-                     * Called when an app open ad has failed to load.
-                     *
-                     * @param loadAdError the error.
-                     */
-                    @Override
-                    public void onAdFailedToLoad(LoadAdError loadAdError) {
-                        // Handle the error.
-                    }
-
-                };
-        AdRequest request = getAdRequest();
+            /**
+             * Called when an app open ad has failed to load.
+             *
+             * @param loadAdError the error.
+             */
+            override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                // Handle the error.
+            }
+        }
+        val request = adRequest
         AppOpenAd.load(
-                myApplication, AD_UNIT_ID, request,
-                AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT, loadCallback);
+            myApplication, AD_UNIT_ID, request,
+            AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT, loadCallback
+        )
     }
 
-    /** Creates and returns ad request. */
-    private AdRequest getAdRequest() {
-        return new AdRequest.Builder().build();
-    }
+    /** Creates and returns ad request.  */
+    private val adRequest: AdRequest
+        private get() = AdRequest.Builder().build()
 
-    /** Utility method that checks if ad exists and can be shown. */
-    public boolean isAdAvailable() {
-        return appOpenAd != null;
-    }
-    /** Shows the ad if one isn't already showing. */
-    public void showAdIfAvailable() {
+    /** Utility method that checks if ad exists and can be shown.  */
+    val isAdAvailable: Boolean
+        get() = appOpenAd != null
+
+    /** Shows the ad if one isn't already showing.  */
+    fun showAdIfAvailable() {
         // Only show ad if there is not already an app open ad currently showing
         // and an ad is available.
-        if (!isShowingAd && isAdAvailable()) {
-            Log.d(LOG_TAG, "Will show ad.");
+        if (!isShowingAd && isAdAvailable) {
+            Log.d(LOG_TAG, "Will show ad.")
+            val fullScreenContentCallback: FullScreenContentCallback =
+                object : FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        // Set the reference to null so isAdAvailable() returns false.
+                        appOpenAd = null
+                        isShowingAd = false
+                        fetchAd()
+                    }
 
-            FullScreenContentCallback fullScreenContentCallback =
-                    new FullScreenContentCallback() {
-                        @Override
-                        public void onAdDismissedFullScreenContent() {
-                            // Set the reference to null so isAdAvailable() returns false.
-                            AppOpenManager.this.appOpenAd = null;
-                            isShowingAd = false;
-                            fetchAd();
-                        }
-
-                        @Override
-                        public void onAdFailedToShowFullScreenContent(AdError adError) {}
-
-                        @Override
-                        public void onAdShowedFullScreenContent() {
-                            isShowingAd = true;
-                        }
-                    };
-
-            appOpenAd.setFullScreenContentCallback(fullScreenContentCallback);
-            appOpenAd.show(currentActivity);
-
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {}
+                    override fun onAdShowedFullScreenContent() {
+                        isShowingAd = true
+                    }
+                }
+            appOpenAd!!.fullScreenContentCallback = fullScreenContentCallback
+            appOpenAd!!.show(currentActivity)
         } else {
-            Log.d(LOG_TAG, "Can not show ad.");
-            fetchAd();
+            Log.d(LOG_TAG, "Can not show ad.")
+            fetchAd()
         }
     }
 
-    /** ActivityLifecycleCallback methods */
-    @Override
-    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+    /** ActivityLifecycleCallback methods  */
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+    override fun onActivityStarted(activity: Activity) {}
+    override fun onActivityResumed(activity: Activity) {}
+    override fun onActivityPaused(activity: Activity) {}
+    override fun onActivityStopped(activity: Activity) {}
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+    override fun onActivityDestroyed(activity: Activity) {}
 
-    }
-
-    @Override
-    public void onActivityStarted(@NonNull Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityResumed(@NonNull Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityPaused(@NonNull Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(@NonNull Activity activity) {
-
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(@NonNull Activity activity) {
-
+    companion object {
+        private const val LOG_TAG = "AppOpenManager"
+        private const val AD_UNIT_ID = "ca-app-pub-6013605396446078/8842820429"
+        private var isShowingAd = false
     }
 }
--- app/src/main/java/com/news/livenews/worldwidenews/view/WebActivity.kt
+++ app/src/main/java/com/news/livenews/worldwidenews/view/WebActivity.kt
@@ -1,174 +1,138 @@
-package com.news.livenews.worldwidenews.view;
+package com.news.livenews.worldwidenews.view
 
-import android.app.Activity;
-import android.app.DownloadManager;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.webkit.CookieManager;
-import android.webkit.DownloadListener;
-import android.webkit.URLUtil;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.app.Activity
+import android.app.DownloadManager
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.webkit.*
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.news.livenews.worldwidenews.R
+import com.news.livenews.worldwidenews.Utils.Fun
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.news.livenews.worldwidenews.R;
-import com.news.livenews.worldwidenews.Utils.Fun;
-
-public class WebActivity extends AppCompatActivity {
-    private WebView webView;
-    String string;
-    private Activity mActivity;
-    private ProgressDialog progressDialog;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_web);
-
-        string = getIntent().getStringExtra("url");
-
-        wevViewfunction();
-        new Fun(this);
-        FrameLayout adContainerView = findViewById(R.id.ad_view_container);
-        Fun.showBannerAds(adContainerView, this);
+class WebActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    var string: String? = null
+    private val mActivity: Activity? = null
+    private val progressDialog: ProgressDialog? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_web)
+        string = intent.getStringExtra("url")
+        wevViewfunction()
+        Fun(this)
+        val adContainerView = findViewById<FrameLayout>(R.id.ad_view_container)
+        Fun.showBannerAds(adContainerView, this)
     }
 
-    private void wevViewfunction() {
+    private fun wevViewfunction() {
+        webView = findViewById(R.id.webViewID)
+        webView.setWebViewClient(browser())
+        webView.setWebChromeClient(GoogleClient())
+        webView.getSettings().javaScriptEnabled = true
+        webView.getSettings().setSupportZoom(true)
+        webView.loadUrl(string!!)
+        webView.getSettings().javaScriptCanOpenWindowsAutomatically = true
+        webView.getSettings().defaultFontSize
+        webView.clearCache(true)
+        webView.clearHistory()
+        webView.getSettings().loadsImagesAutomatically = true
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY)
+        webView.setDownloadListener(DownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimetype)
+            val cookies = CookieManager.getInstance().getCookie(url)
+            request.addRequestHeader("cookie", cookies)
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(
+                URLUtil.guessFileName(
+                    url, contentDisposition,
+                    mimetype
+                )
+            )
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalFilesDir(
+                this@WebActivity,
+                Environment.DIRECTORY_DOWNLOADS, ".pdf"
+            )
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(
+                applicationContext, "Downloading File",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+        webView.setOnClickListener(View.OnClickListener { webView.loadUrl(string!!) })
+    }
 
-        webView = findViewById(R.id.webViewID);
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.web_menu, menu)
+        return true
+    }
 
-        webView.setWebViewClient(new browser());
-        webView.setWebChromeClient(new GoogleClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setSupportZoom(true);
-        webView.loadUrl(string);
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webView.getSettings().getDefaultFontSize();
-        webView.clearCache(true);
-        webView.clearHistory();
-        webView.getSettings().setLoadsImagesAutomatically(true);
-        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-
-                DownloadManager.Request request = new
-                        DownloadManager.Request(Uri.parse(url));
-                request.setMimeType(mimetype);
-                String cookies = CookieManager.getInstance().getCookie(url);
-                request.addRequestHeader("cookie", cookies);
-                request.addRequestHeader("User-Agent", userAgent);
-                request.setDescription("Downloading file...");
-                request.setTitle(URLUtil.guessFileName(url, contentDisposition,
-                        mimetype));
-                request.allowScanningByMediaScanner();
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                request.setDestinationInExternalFilesDir(WebActivity.this,
-                        Environment.DIRECTORY_DOWNLOADS, ".pdf");
-
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                dm.enqueue(request);
-
-                Toast.makeText(getApplicationContext(), "Downloading File",
-                        Toast.LENGTH_LONG).show();
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.goBack -> if (webView!!.canGoBack()) {
+                webView!!.goBack()
             }
-        });
-
-        webView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                webView.loadUrl(string);
+            R.id.goForward -> if (webView!!.canGoForward()) {
+                webView!!.goForward()
             }
-        });
+            R.id.reload -> webView!!.loadUrl(webView!!.url.toString())
+            R.id.share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Install now")
+                val app_url = ""
+                shareIntent.putExtra(Intent.EXTRA_TEXT, app_url)
+                startActivity(Intent.createChooser(shareIntent, "Share via"))
+            }
+            R.id.rate -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+                startActivity(intent)
+            }
+            R.id.moreapp -> {
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+                startActivity(i)
+            }
+            R.id.exit -> {
+                finish()
+                System.exit(0)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.web_menu, menu);
-        return true;
-    }
-
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.goBack:
-                if (webView.canGoBack()) {
-                    webView.goBack();
-                }
-                break;
-            case R.id.goForward:
-
-                if (webView.canGoForward()) {
-                    webView.goForward();
-                }
-                break;
-            case R.id.reload:
-
-                webView.loadUrl(webView.getUrl().toString());
-                break;
-            case R.id.share:
-                Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Install now");
-                String app_url = "";
-                shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, app_url);
-                startActivity(Intent.createChooser(shareIntent, "Share via"));
-                break;
-            case R.id.rate:
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(""));
-                startActivity(intent);
-                break;
-            case R.id.moreapp:
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(""));
-                startActivity(i);
-                break;
-            case R.id.exit:
-                finish();
-                System.exit(0);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public class browser extends WebViewClient {
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-
-            super.onPageStarted(view, url, favicon);
+    inner class browser : WebViewClient() {
+        override fun onPageStarted(view: WebView, url: String, favicon: Bitmap) {
+            super.onPageStarted(view, url, favicon)
         }
 
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
         }
 
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
         }
     }
 
-
-    private class GoogleClient extends WebChromeClient {
-
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
+    private inner class GoogleClient : WebChromeClient() {
+        override fun onProgressChanged(view: WebView, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
         }
     }
 }
